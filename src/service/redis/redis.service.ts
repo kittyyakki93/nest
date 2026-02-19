16252d08b5cca66b1b9f71aff8dca036671a532d
김하림 <kittyyakki93@gmail.com>
2026-02-19 20:53:04 +0900

    fix

--- src/service/redis/redis.service.ts
+++ src/service/redis/redis.service.ts
@@ -27,35 +27,32 @@ export class RedisService {
   // value -> refreshToken
   // ttl -> 만료시간
   async setRefreshToken(payload: JwtPayload, token: string, ttl: number) {
-    const key = `${this.REFRESH_PREFIX}${payload.id}`
-    await this.redisClient.set(key, token, {EX: ttl})
+    const key = `${this.REFRESH_PREFIX}${payload.id}`;
+    await this.redisClient.set(key, token, { EX: ttl });
   }
 
   // 2. Redis에 저장된 RefreshToken이 유효한지 확인
-  async getRefreshToken(payload: JwtPayload) : Promise<string | null> {
+  async getRefreshToken(payload: JwtPayload): Promise<string | null> {
     const key = `${this.REFRESH_PREFIX}${payload.id}`;
-    return await this.redisClient.get(key)
+    return await this.redisClient.get(key);
   }
 
   // 3. Redis에 블랙리스트 등록(예: JWT 토큰을 Key로 저장)
   async addToBlackList(token: string, ttl: number) {
-    const key = `${this.BLACKLIST_PREFIX}${token}`
-    await this.redisClient.set(key, 'true', {EX: ttl})
+    const key = `${this.BLACKLIST_PREFIX}${token}`;
+    await this.redisClient.set(key, 'true', { EX: ttl });
   }
 
   // 4. Redis에 블랙리스트로 등록되어 있는지 확인
   async isBlackListed(token: string) {
     const key = `${this.BLACKLIST_PREFIX}${token}`;
     const result = await this.redisClient.get(key);
-    return result === 'true'
+    return result === 'true';
   }
 
   // 5. Redis에 등록된 RefreshToken을 무효화
-  async deleteRefreshToken(payload: JwtPayload){
+  async deleteRefreshToken(payload: JwtPayload) {
     const key = `${this.REFRESH_PREFIX}${payload.id}`;
-    await this.redisClient.del(key)
+    await this.redisClient.del(key);
   }
-
-
-
 }
